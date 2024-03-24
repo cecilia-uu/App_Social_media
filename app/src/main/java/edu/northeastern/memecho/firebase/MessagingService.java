7aec55ce149a1186c25fab68e732ac5774dfc9f2
cecilia-uu <huikong1996@163.com>
2024-03-23 23:02:08 -0700

    added firebase setting

---
+++ app/src/main/java/edu/northeastern/memecho/firebase/MessagingService.java
@@ -0,0 +1,24 @@
+package edu.northeastern.memecho.firebase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Objects;
+
+public class MessagingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String token) {
+        super.onNewToken(token);
+        Log.d("FCM", "Token: " + token);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+        Log.d("FCM", "Message: " + Objects.requireNonNull(message.getNotification()).getBody());
+    }
+}
