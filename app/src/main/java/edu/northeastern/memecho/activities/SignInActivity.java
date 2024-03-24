472561c152cd10cb120023973de71e7a4ab2f4a8
cecilia-uu <huikong1996@163.com>
2024-03-23 18:59:17 -0700

    completed the function of signing up and signing in

--- app/src/main/java/edu/northeastern/memecho/activities/SignInActivity.java
+++ app/src/main/java/edu/northeastern/memecho/activities/SignInActivity.java
@@ -2,15 +2,26 @@ package edu.northeastern.memecho.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import edu.northeastern.memecho.R;
+import edu.northeastern.memecho.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+    // the binding class for each XML layout will be generated automatically
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
     }
 }
--- app/src/main/java/edu/northeastern/memecho/activities/SignUpActivity.java
+++ app/src/main/java/edu/northeastern/memecho/activities/SignUpActivity.java
@@ -2,15 +2,25 @@ package edu.northeastern.memecho.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import edu.northeastern.memecho.R;
+import edu.northeastern.memecho.databinding.ActivitySignInBinding;
+import edu.northeastern.memecho.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v ->
+                finish());
     }
 }
