d41899a97b90b0ae552c875ffa9201bafa5d7d54
cecilia-uu <huikong1996@163.com>
2024-03-25 15:29:14 -0700

    created new chatActivity

---
+++ app/src/main/java/edu/northeastern/memecho/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.memecho.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.memecho.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
